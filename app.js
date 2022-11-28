53e04d2be9440a8572ab270209abf6315a8adfe5
noman318 <shaikhmohdnoman612@gmail.com>
2022-11-28 10:52:33 +0530

    Added table to collect data of form submitted

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 console.log("My_Pug_UI");
 const express = require("express");
 const app = express();
-const fs= require('fs');
+const fs = require("fs");
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 const port = 3500;
 
 app.set("view engine", "pug");
@@ -11,7 +11,6 @@ app.set("views", "./views");
 app.get("/", (req, res) => {
   res.render("index");
 });
-
 
 app.get("/contact", (req, res) => {
   res.render("contact");
@@ -26,31 +25,40 @@ app.get("/aboutus", (req, res) => {
   res.render("aboutus");
 });
 app.get("/details", (req, res) => {
-  res.sendFile('contact.txt',{root:'./'});
+  res.render("details");
 });
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-
-app.post("/postdata",(req,res)=>{
-  let name= req.body.name;
-  let email = req.body.email;
-  let subject= req.body.subject;
-  let comments= req.body.comments
-  const data = {name:req.body.name,email:req.body.email}
-  fs.appendFile('contact.txt', `${JSON.stringify(data)}\n` , function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-    res.sendFile('contact.txt',{root:'./'});
-
-  });
-})
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
   } else {
     console.log(`server in running on port ${port}`);
+  }
+});
+
+app.post("/postdata", (req, res) => {
+  let name = req.body.name;
+  let email = req.body.email;
+  let subject = req.body.subject;
+  let comments = req.body.comments;
+  let data = `
+  <tr>
+    <td>${name}</td>
+    <td>${email}</td>
+    <td>${subject}</td>
+    <td>${comments}</td>
+  <tr>
+      `;
+  if (fs.existsSync(`./user`)) {
+    fs.appendFileSync(`./user/detail.pug`, data);
+    res.render("form-submitted");
+  } else {
+    fs.mkdirSync(`./user/`);
+    fs.writeFileSync(`./user/detail.pug`, `${data.toString()}`);
+    res.send("CREATE FILE OF CONTACT");
   }
 });
